b746967c2299ffff2e4d0e87736efc91c8c5595d
Ray Lin <raylin009@gmail.com>
2021-02-09 21:53:08 -0800

    added material ui

--- src/Components/QuestionCard.js
+++ src/Components/QuestionCard.js
@@ -1,13 +1,22 @@
 import { useState } from 'react';
+import Button from '@material-ui/core/Button'
+
+
+
 const Question = () => {
   const [count, setCount] = useState(0);
+  
 
   return (
     <div>
       <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>
+      <Button 
+        onClick={() => setCount(count + 1)} 
+        variant="contained" 
+        color="primary"
+      >
         Add 1
-      </button>
+    </Button>
     </div>
   )
 }
