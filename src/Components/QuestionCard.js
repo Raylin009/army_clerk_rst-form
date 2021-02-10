3e029ce4bdfb01c16783a6caa022af5d1b637056
Ray Lin <raylin009@gmail.com>
2021-02-09 22:01:41 -0800

    added text field

--- src/Components/QuestionCard.js
+++ src/Components/QuestionCard.js
@@ -1,11 +1,20 @@
 import { useState } from 'react';
-import Button from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 const Question = () => {
   const [count, setCount] = useState(0);
-  
+  const classes = useStyles();
 
   return (
     <div>
@@ -16,7 +25,12 @@ const Question = () => {
         color="primary"
       >
         Add 1
-    </Button>
+      </Button>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="standard-basic" label="Standard" />
+        <TextField id="filled-basic" label="Filled" variant="filled" />
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </form>
     </div>
   )
 }
